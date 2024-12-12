f67d7db76a82ff8dd466ed26e3f85332775ef296
Kevin <kg71875@gmail.com>
2024-12-12 12:11:13 +0100

    Props.1 fait

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
 import PokemonCard from "./components/PokemonCard";
-
+// const pokemon = pokemonList[0];
 function App() {
-  return (
-  <PokemonCard/>
-)
+  const pokemonList = [
+    {
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
+  return <PokemonCard pokemon={pokemonList[0]} />;
 }
 
 export default App;
--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,23 +1,27 @@
-export default function PokemonCard() {
-  const pokemon = pokemonList[1];
-  return(
-  <>
-  <figure>
-  {pokemon.imgSrc ? <img src = {pokemon.imgSrc} alt={pokemon.name}/> : <p>???</p>} 
-  <figcaption>Bulbasaur</figcaption>
-  </figure>
-  </>
-)}
+export default function PokemonCard({ pokemon }) {
+  // const pokemon = pokemonList[0];
+  // const pokemon = { name: "mew", imgSrc: null };
+  return (
+    <>
+      <figure>
+        {pokemon.imgSrc ? (
+          <img src={pokemon.imgSrc} alt={pokemon.name} />
+        ) : (
+          <p>???</p>
+        )}
+        <figcaption>Bulbasaur</figcaption>
+      </figure>
+    </>
+  );
+}
 
-
-
-const pokemonList = [
-  {
-    name: "bulbasaur",
-    imgSrc:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-  },
-  {
-    name: "mew",
-  },
-];
+// const pokemonList = [
+//   {
+//     name: "bulbasaur",
+//     imgSrc:
+//       "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+//   },
+//   {
+//     name: "mew",
+//   },
+// ];
